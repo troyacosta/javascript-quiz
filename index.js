828903ef9ecde16e0febb6d35a24c56c245537c5
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-01 14:08:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,143 @@
+var prompt = require('prompt-sync').prompt;
+var correct = 0;
+
+
+console.log('Let\'s take a short quiz to test your knowledge. What\'s your name?');
+var name = prompt();
+console.log('Hi '+name+' \, let\'s get started!');
+console.log('What should be the first tag at the top of your html file? head, html, or doctype?');
+var answer = prompt();
+if (answer.toLowerCase() === 'doctype') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('Which \'H\' tag will give you the biggest font? h1, h2, or h6?');
+var answer = prompt();
+if (answer.toLowerCase() === 'h1') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('How many times should you use an ID? one, two, or three?');
+var answer = prompt();
+if (answer.toLowerCase() === 'one') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('What type of element is a \"span\"? Block, inline, or floated?');
+var answer = prompt();
+if (answer.toLowerCase() === 'inline') {
+	correct++
+	console.log('That\'s correct!')
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('Can you style elements in your html? Yes or no.');
+var answer = prompt();
+if (answer.toLowerCase() === 'yes') {
+	correct++
+	console.log('That\'s correct, but you shouldn\'t!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+console.log('');
+var answer = prompt();
+if (answer.toLowerCase() === '') {
+	correct++
+	console.log('That\'s correct!');
+}
+else {
+	console.log('Sorry, that\'s incorrect.');
+}
+
